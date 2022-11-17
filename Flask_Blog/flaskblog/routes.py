d1f69a0e462edbd3dfbb54f42d05f8eb525d9898
audualiyuehezu <alidevopsolid@outlook.com>
2022-11-17 13:44:44 -0500

    app update

--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app,db,bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user,login_required
 
 
 
@@ -74,5 +74,6 @@ def logout():
     return redirect(url_for('home'))
 
 @app.route("/account")
+@login_required
 def account():
-    return redirect(url_for('account', title='Account'))
+    return render_template('account.html', title='Account')
