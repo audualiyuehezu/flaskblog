4a94b9ae47901e3b453513b37c9a9366a7b248b7
audualiyuehezu <alidevopsolid@outlook.com>
2022-11-22 02:17:22 -0500

    app update

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -17,11 +17,11 @@ bcrypt = Bcrypt(app)
 login_manager =LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
 app.config['MAIL_PORT'] = '587'
 app.config['MAIL_USE_TLS'] = 'TLS'
-app.config['MAIL_USERNAME'] = 'email'
-app.config['MAIL_PASSWORD'] = 'password'
+app.config['MAIL_USERNAME'] = 'alidevopsolid@outlook.com'
+app.config['MAIL_PASSWORD'] = 'Iamwhoiamyoucantbeme'
 
 mail = Mail(app)
 
